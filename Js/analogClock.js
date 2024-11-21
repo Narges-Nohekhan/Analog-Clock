437c6ce8774e5856958cc348be4f5bc23417abe7
Narge <nargesnovhekhan@gmail.com>
2024-11-21 17:28:45 +0330

    the end of the clock project

--- Js/analogClock.js
+++ Js/analogClock.js
@@ -3,14 +3,14 @@ let min = document.querySelector('.min');
 let hour = document.querySelector('.hour');
 let deg = 6;
 
-setInterval(clockHandler , 1000);
+setInterval(clockHandler, 1000);
 
-function clockHandler(){
+function clockHandler() {
     let d = new Date();
     let h = d.getHours() * 30;
     let m = d.getMinutes() * deg;
     let s = d.getSeconds() * deg;
-    hour.style.transform = `rotateZ(${h + (m/12)}deg)`;
+    hour.style.transform = `rotateZ(${h + (m / 12)}deg)`;
     sec.style.transform = `rotateZ(${s}deg)`;
     min.style.transform = `rotateZ(${m}deg)`;
 }
